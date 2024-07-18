477708fd86016c1cc0baf67a85dd9a16d8a2f179
Leo Boissard <lsboissard@gmail.com>
2024-07-18 20:10:58 -0300

    add initials configurations for entityframework

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,11 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+string? connectionString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connectionString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
